59b321b71f4f209a611c686d126fd41f81fcf5e7
Artyom <itcoding76@gmail.com>
2022-12-24 10:26:50 +0200

    Task 2 is solved

--- Program.cs
+++ Program.cs
@@ -10,34 +10,34 @@ m = 3, n = 4.
 
 8 7,8 -7,1 9
 */
-//вопрос по первой задачи, как вывести вещественные числа без занка после запятой 
+//вопрос по первой задачи, как вывести вещественные числа без знака после запятой 
 //================= Task 1 27min ===========================
 
-void CreateAndShow2DArray(int rows, int cols, int min, int max)
-{
-    double[,] array = new double[rows, cols];
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            array[i, j] = new Random().NextDouble() + new Random().Next(min, max);
-            Console.Write($"{array[i, j]:0.0} ");
-        }
-        Console.WriteLine();
-    }
-}
+// void CreateAndShow2DArray(int rows, int cols, int min, int max)
+// {
+//     double[,] array = new double[rows, cols];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < cols; j++)
+//         {
+//             array[i, j] = new Random().NextDouble() + new Random().Next(min, max);
+//             Console.Write($"{array[i, j]:0.0} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-Console.Write($"Enter number of rows: ");
-int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"Enter number of cols: ");
-int columns = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Enter number of cols: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"Enter min: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write($"Enter max: ");
-int max = Convert.ToInt32(Console.ReadLine());
-CreateAndShow2DArray(rows, columns, min, max);
+// Console.Write($"Enter min: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Enter max: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// CreateAndShow2DArray(rows, columns, min, max);
 
 /*
 Задача 50. Напишите программу, которая на вход 
@@ -55,7 +55,52 @@ CreateAndShow2DArray(rows, columns, min, max);
 
 17 -> такого числа в массиве нет
 */
-//================= Task 2 ===========================
+//================= Task 2 25 min ===========================
+
+int[,] Create2DArray(int rows, int cols, int min, int max)
+{
+    int[,] array = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            array[i,j] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void Show2DArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j< array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindArrayElementPosition(int[,] array, int row, int col)
+{
+    if(row >= array.GetLength(0) || col >= array.GetLength(1)) 
+        Console.Write("A number is absent in array.");
+    else 
+    {
+        Console.Write($"Value is {array[row, col]}");
+    }
+}
+int[,] array  = Create2DArray(4, 4, 1, 9);
+Show2DArray(array);
+
+Console.Write("Enter row`s number: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter column`s number");
+int column = Convert.ToInt32(Console.ReadLine());
+
+FindArrayElementPosition(array, row, column);
+
 
 /*
 Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
